ca30fd3a03532cdb043b9cec502db8e7d1342060
Roman <romex.ua@gmail.com>
2019-09-20 13:06:42 +0300

    numbers

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,14 +1,27 @@
 package com.epam;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello");
-        for(int i=3; i<=10 ; i++) {
-            System.out.println("3 * " + i + " " + "= " + i*3);
+        System.out.println("darova daun");
+        System.out.println("давай вводь число, а я його суммую з усіма числами до нього:");
+        Scanner input = new Scanner(System.in);
+        int value= input.nextInt();
+        int sum = 0;
+        for (int i=1;i<=value;i++){
+            sum = sum + i;}
+        System.out.println("Мій магічний шар показує: " + sum); //copyright дєлай
+    }
+    //public static void main(String[] args) {
+       // System.out.println("enter a number");
+        //for (int i = 3; i <= 10; i++) {
+
+           // Scanner input = new Scanner(System.in);
+           // int value = input.nextInt();
+           // System.out.println(value); }
+
     }
 
-
-    }
-}
 
 
