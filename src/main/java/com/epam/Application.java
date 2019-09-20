15634e37fb541e07326bddcc83280a396170a535
Roman <romex.ua@gmail.com>
2019-09-20 13:18:04 +0300

    зірочки

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("darova daun");
-        System.out.println("давай вводь число, а я його суммую з усіма числами до нього:");
-        Scanner input = new Scanner(System.in);
-        int value= input.nextInt();
-        int sum = 0;
-        for (int i=1;i<=value;i++){
-            sum = sum + i;}
-        System.out.println("Мій магічний шар показує: " + sum); //copyright дєлай
+        for (int i=1;i<=3;i++){
+            System.out.println("");
+            for (int o=1;o<=5;o++)
+                System.out.print("* ");
+            }
+       // System.out.println("darova vsem");
+       // System.out.println("давай вводь число, а я його суммую з усіма числами до нього:");
+       // Scanner input = new Scanner(System.in);
+       // int value= input.nextInt();
+       // int sum = 0;
+       // for (int i=1;i<=value;i++){
+       //     sum = sum + i;}
+       // System.out.println("Мій магічний шар показує: " + sum); //copyright дєлай
     }
     //public static void main(String[] args) {
        // System.out.println("enter a number");
