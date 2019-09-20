e2dd3be88f9305b502326c5e1ecc6c294b7b8240
Roman <romex.ua@gmail.com>
2019-09-20 12:33:23 +0300

    table

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -3,9 +3,12 @@ package com.epam;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
+        for(int i=3; i<=10 ; i++) {
+            System.out.println("3 * " + i + " " + "= " + i*3);
     }
 
 
+    }
 }
 
 
